10a1d2e1cefa2623b8222cb15362072f88344ca9
Wheelman-v2 <149758344+Wheelman-v2@users.noreply.github.com>
2024-02-15 15:29:34 +0900

    Update filelink.js

--- filelink.js
+++ filelink.js
@@ -2,6 +2,9 @@
       var baseUrl = 'https://github.com/Wheelman-v2/Public_institution_Fonts/raw/main';
       var specificPaths = {
         '62570': 'MPVA/62570%20ttf_Typeface%20manual.zip',
+        'Sokcho_Bada_Dotum': 'Sokcho-si/Sokcho_Bada_Dotum.zip',
+        'Sokcho_Bada_Batang': 'Sokcho-si/Sokcho_Bada_Batang.zip',
+        'Sokcho_Bada_Calligraphy': 'Sokcho-si/Sokcho_Bada_Calligraphy.zip',
         // 'font2': 'AnotherFolder/another_font.zip',
         // Add more link types as needed
       };
